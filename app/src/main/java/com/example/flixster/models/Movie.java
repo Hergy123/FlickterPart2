52174295aae8b32fbf8df61e98e548ea0bbda132
Hergy123 <hergyj55!!@gmail.com>
2020-03-26 22:16:44 -0400

    this is my App

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -4,16 +4,26 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Parcel
 public class Movie {
+    int movieId;
 
     String backDropath;
     String poterPath;
     String title;
     String overview;
+    double rating ;
+
+    // empty constructor
+    public Movie (){
+
+    }
 
 
     public Movie(JSONObject jsonObject) throws JSONException {
@@ -21,6 +31,8 @@ public class Movie {
         poterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+       rating = jsonObject.getDouble("vote_average");
+       movieId = jsonObject.getInt("id");
     }
 
     public String getPoterPath() {
@@ -33,7 +45,16 @@ public class Movie {
     }
 
     public String getOverview() {
+
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 
     public String getBackDropath() {
